378cdbab015b6561b10be57e8bb9460a3da320c0
Gustave Ronteix <gronteix@users.noreply.github.com>
2020-04-11 17:56:10 +0200

    include airplane in prep data

--- datasets/chorus-dt/scripts/download_data.sh
+++ datasets/chorus-dt/scripts/download_data.sh
@@ -1,11 +1,11 @@
 # Avion train sample
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1xD9hsfKrlh5rJoGaYqkK_PRUaFQSZmfk' -O ./data/raw/avion-train.csv
+curl --insecure 'https://docs.google.com/uc?export=download&id=1xD9hsfKrlh5rJoGaYqkK_PRUaFQSZmfk' -O ./data/raw/avion-train.csv
 # Laposte codes
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1EFDXoxmiXVGHwsi6aIPLXQxoMoO9y7ut' -O ./data/raw/laposte_hexasmal.csv
+curl --insecure 'https://docs.google.com/uc?export=download&id=1EFDXoxmiXVGHwsi6aIPLXQxoMoO9y7ut' -O ./data/raw/laposte_hexasmal.csv
 # Lists of train stations
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1zLRRqbHNH4y487JKoBA-GhfPSZt-7GhF' -O ./data/raw/liste-des-gares.csv
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1WSAj7khA-HGjTyHpWbAXzZs2JGafNkfD' -O ./data/raw/referentiel-gares-voyageurs.csv
+curl --insecure 'https://docs.google.com/uc?export=download&id=1zLRRqbHNH4y487JKoBA-GhfPSZt-7GhF' -O ./data/raw/liste-des-gares.csv
+curl --insecure 'https://docs.google.com/uc?export=download&id=1WSAj7khA-HGjTyHpWbAXzZs2JGafNkfD' -O ./data/raw/referentiel-gares-voyageurs.csv
 # Gmap cache
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1iw8q0Ve5AMusts11GrI2XzmOAbfKaclM' -O ./data/raw/gmap_responses.pkl
+curl --insecure 'https://docs.google.com/uc?export=download&id=1iw8q0Ve5AMusts11GrI2XzmOAbfKaclM' -O ./data/raw/gmap_responses.pkl
 
 
