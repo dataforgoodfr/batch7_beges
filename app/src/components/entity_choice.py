d6eea39767ebbe8db941923fe90c66f928a733b0
Antoine <antoine.biard.10@gmail.com>
2020-04-30 16:38:21 +0200

    Fix navigation issues

--- app/src/components/entity_choice.py
+++ app/src/components/entity_choice.py
@@ -6,6 +6,7 @@ from utils.entity_handler import EntityHandler
 
 eh = EntityHandler()
 print(eh.get_level_1_dropdown_items())
+level_2_style = {"margin": "10px", "display": "none"}
 
 layout = html.Div(
     id="div-entity-choice",
@@ -13,47 +14,39 @@ layout = html.Div(
         dcc.Dropdown(
             id="dropdown-entity-choice-level-1",
             options=eh.get_level_1_dropdown_items(),
-            # value=eh.get_level_1_dropdown_items()[0]["value"],
             placeholder="Choisissez votre organisation",
             style={"margin": "10px"},
         ),
-        dcc.Dropdown(
-            id="dropdown-entity-choice-level-2", placeholder="Choisissez votre service", style={"margin": "10px"}
-        ),
-        html.Div(
-            id="div-selected-entity",
-            children=[html.P("Vous avez selectionné : "), html.P(id="p-selected-entity"),],
-            style={"display": " inline-block"},
-        ),
+        dcc.Dropdown(id="dropdown-entity-choice-level-2", placeholder="Choisissez votre service", style=level_2_style),
+        html.Div(id="selected-entity", style={"display": "none"}),
     ],
 )
 
 
 def register_callbacks(app):
     @app.callback(
-        [Output("dropdown-entity-choice-level-2", "options"), Output("dropdown-entity-choice-level-2", "value"),],
-        [Input("dropdown-entity-choice-level-1", "value")],
+        [
+            Output("dropdown-entity-choice-level-2", "style"),
+            Output("dropdown-entity-choice-level-2", "options"),
+            Output("selected-entity", "children"),
+        ],
+        [Input("dropdown-entity-choice-level-1", "value"), Input("dropdown-entity-choice-level-2", "value")],
     )
-    def change_dropdown_level_2_options(value):
-        if value is not None:
-            options = eh.get_level_2_dropdown_items(value)
-            level_2_value = options[0]["value"]
-        else:
-            options = []
-            level_2_value = None
-        return options, level_2_value
-
-    @app.callback(
-        Output("p-selected-entity", "children"),
-        [Input("dropdown-entity-choice-level-1", "value"), Input("dropdown-entity-choice-level-2", "value"),],
-    )
-    def display_selected_entity(level_1, level_2):
-        if level_1 is None:
-            level_1_label = ""
-        else:
-            level_1_label = eh.get_level_1_label(level_1)
-        if level_2 is None:
-            level_2_label = ""
-        else:
-            level_2_label = eh.get_level_2_label(level_2)
-        return level_1_label + " / " + level_2_label
+    def on_dropdown_level_1_value(value_level_1, value_level_2):
+        options = []
+        style = level_2_style
+        selected_entity = None
+        if value_level_1 is not None:
+            if value_level_2 is None:
+                if value_level_1 != "AC":
+                    options = eh.get_level_2_dropdown_items(value_level_1)
+                    style = level_2_style.copy()
+                    style["display"] = "block"
+                elif value_level_1 == "AC":
+                    selected_entity = "AC;"
+            else:
+                options = eh.get_level_2_dropdown_items(value_level_1)
+                style = level_2_style.copy()
+                style["display"] = "block"
+                selected_entity = ";".join((value_level_1, value_level_2))
+        return style, options, selected_entity
--- app/src/components/header.py
+++ app/src/components/header.py
@@ -5,21 +5,26 @@ layout = html.Div(
     children=[
         html.H1("Outil d'aide à la création de BEGES", style={"text-align": "center"}),
         html.Hr(),
-        html.P(
-            "Cet outil a été crée dans le crade de la saison 7 de data4good afin d'aider les différents services à créer leur Bilans d'émission de gaz à effet de serre (BEGES).",
-            style={"text-align": "center"},
-        ),
-        html.P(
-            "Différentes bases de données internes aux administrations, et identifiées comme principaux postes d'émissions de gaz à effet de serre sont exploitées.",
-            style={"text-align": "center"},
-        ),
-        html.Ul(
-            [
-                html.Li("Chorus-dt: déplacements contenant des trajets en train et avion"),
-                html.Li("OSFI: dépenses énergétiques des batiments"),
-                html.Li("O-Drive: déplacements en voiture"),
+        html.Div(
+            children=[
+                html.P(
+                    "Cet outil a été créé dans le crade de la saison 7 de data4good afin d'aider les différents services à créer leur Bilans d'émission de gaz à effet de serre (BEGES).",
+                    style={"text-align": "center"},
+                ),
+                html.P(
+                    "Cette application permet d'exploiter les principaux postes d'émissions de gaz à effet de serre detéctés :",
+                    style={"text-align": "center"},
+                ),
+                html.Ul(
+                    [
+                        html.Li("Déplacements en train / avion,"),
+                        html.Li("Déplacements en voiture,"),
+                        html.Li("Dépenses énergétiques des batiments."),
+                    ],
+                    style={"width": "50%", "margin": "auto"},
+                ),
             ],
-            style={"width": "50%", "margin": "auto"},
+            style={"margin": "auto", "width": "50%"},
         ),
     ],
 )
