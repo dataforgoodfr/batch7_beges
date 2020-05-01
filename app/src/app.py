28b61e5efefeb567fc812773b409b50ed3c94aea
Ayoub <ayoubsamaki@gmail.com>
2020-05-01 21:58:44 +0200

    Changes in layout struture for chorus-dt

--- app/src/app.py
+++ app/src/app.py
@@ -1,6 +1,7 @@
 import dash
+import dash_bootstrap_components as dbc
 
-external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+external_stylesheets = [dbc.themes.BOOTSTRAP] #["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = "Beges"
--- app/src/components/chorus_dt.py
+++ app/src/components/chorus_dt.py
@@ -40,21 +40,56 @@ def make_emissions_timeseries():
                       )
     return fig
 
+select_prestation_type = dcc.Dropdown(
+    id='select-prestation_type',
+    options=[{'label': 'Train', 'value': 'T'}, {'label': 'Avion', 'value': 'A'}]
+)
+
 card_indicateur = dbc.Card(dbc.CardBody([
-    html.P("Nombre de trajets"),
-    dbc.Row(html.H3('2 300'))
+    html.H3("Nombre de trajets"),
+    html.H3('2 300')
 ]), className='pretty_container')
+
+def make_card_indicateur(title, value):
+    return dbc.Card(dbc.CardBody([
+        html.P(title),
+        html.H3(value)
+    ]), className='pretty_container')
+
+cards = dbc.CardDeck([
+    make_card_indicateur("Nombre de trajets", "2 300"),
+    make_card_indicateur("Emissions (eCO2)", "2M"),
+    make_card_indicateur("Indicateur X", "XX"),
+    make_card_indicateur("Indicateur Y", "YY"),
+])
 
 layout = html.Div([
     dbc.Row(html.P('', id='values-selected')),
     # Cards row
     dbc.Row([
-        dbc.Col(),
         dbc.Col([
-            dbc.Row(card_indicateur),
+                html.B('', id='selected-entity-show'),
+                dbc.Card(dbc.CardBody([
+                    html.H3('Filtres'),
+                    html.Br(),
+                    dbc.FormGroup([
+                        dbc.Label("Type de prestation"),
+                        select_prestation_type
+                    ])
+
+                ]), className='pretty_container'),
+                dbc.Card(dbc.CardBody([
+                    html.H3('Exporter les données'),
+                    html.Br(),
+                    dbc.Button('Export', id='export')
+                ]), className='pretty_container'),
+                dbc.Jumbotron("Explications sur les graphiques et leur fonctionnement..."),
+        ]),
+        dbc.Col([
+            cards,
             build_figure_container(title='Évolution temporelles des émissions', id='timeseries-chorus-dt', \
                                    footer='Explications..'),
-        ], width=8)
+        ], width=9)
     ]),
     dbc.Row([
         dbc.Col([
@@ -69,6 +104,15 @@ layout = html.Div([
     ])
 ], id="div-data-chorus-dt")
 
+@app.callback(
+    Output("selected-entity-show", "children"), [Input("selected-entity", "children")]
+)
+def on_selected_entity_fill_tabs_data(selected_entity):
+    if selected_entity is not None:
+        organisation, service = selected_entity.split(";")
+        return "Organisation : " + organisation + ", Service : " + service
+    else:
+        return "empty"
 
 @app.callback(Output("values-selected'", "children"), [Input("selected-entity", "children")])
 def display_graphs(selected_entity):
