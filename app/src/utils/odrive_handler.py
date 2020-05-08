d90bc642b76c1d6c275c3898b1bc2825db533625
ArtusVuatrin <a.vuatrin@yahoo.fr>
2020-05-08 20:04:16 +0200

    Add handeler

---
+++ app/src/utils/odrive_handler.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+class OdriveHandler:
+    """"
+        Class for loading Odrive data and returning
+    """
+
+    def __init__(self):
+        self.data = pd.read_csv("/data/cleaned/odrive.csv")
+
+    def get_structure_data(self, code_structure=None):
+        if code_structure is not None:
+            return self.data.loc[self.data.code_structure == code_structure, :]
+        return self.data
+
+
+ov = OdriveHandler()
