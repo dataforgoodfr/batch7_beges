3d556ecb32ea98bdf2b71a95d632ca4a62ac6834
ArtusVuatrin <a.vuatrin@yahoo.fr>
2020-05-08 11:45:08 +0200

    Changes on download and modify file

--- app/src/scripts/prepare_datasets.py
+++ app/src/scripts/prepare_datasets.py
@@ -1,7 +1,9 @@
 from datasets.chorus_dt.codes_preparation import main as chorus_dt_prepare_codes
 from datasets.chorus_dt.places_resolution import main as chorus_dt_resolve_places
+from datasets.odrive.file_conversion import main as odrive_file_conversion
 
 if __name__ == "__main__":
     # Prepare chorus dt
+    odrive_file_conversion()
     chorus_dt_prepare_codes()
     chorus_dt_resolve_places()
