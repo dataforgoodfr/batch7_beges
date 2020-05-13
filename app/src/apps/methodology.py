0b67991579626408e286539d7291e75908f545d0
Antoine <antoine.biard.10@gmail.com>
2020-05-13 13:03:05 +0200

    Fix indents

--- app/src/apps/methodology.py
+++ app/src/apps/methodology.py
@@ -3,35 +3,37 @@ import dash_core_components as dcc
 
 intro = dcc.Markdown(
     """
-    # Introduction
-    L'objectif de cette application est de centraliser les données pour les différentes entitées... (on explique qu'on récupère les différents datasets et tout et tout)
-    """
+# Introduction
+L'objectif de cette application est de centraliser les données pour les différentes entitées... (on explique qu'on récupère les différents datasets et tout et tout)
+"""
 )
 methodology_odrive = dcc.Markdown(
     """
-    # Déplacement en voiture
-    Et voilà, on a récupéré la base de données Odrive et puis c'est tout.
-    """
+# Déplacement en voiture
+Et voilà, on a récupéré la base de données Odrive et puis c'est tout.
+"""
 )
 methodology_chorus_dt = dcc.Markdown(
     """
-    # Déplacement en train / avion
-    On a récupéré la base de données Chorus dt qui centralise tout ca.
-    Puis on a résolu les noms d'endroit en utilisant l'API de gmap.
-    Et on a utilisé les facteurs d'émissions suivants :
-    -
-    -
-    """
+# Déplacement en train / avion
+On a récupéré la base de données Chorus dt qui centralise tout ca.
+Puis on a résolu les noms d'endroit en utilisant l'API de gmap.
+
+Et on a utilisé les facteurs d'émissions suivants :
+- Facteur 1
+- Facteur 2
+"""
 )
 
 methodology_osfi = dcc.Markdown(
     """
-    # Dépenses énergétiques
-    On a récupéré la base de données OSFI uniquement en production ici et là.
-    Et on a utilisé les facteurs d'émissions suivants :
-    -
-    -
-    """
+# Dépenses énergétiques
+On a récupéré la base de données OSFI uniquement en production ici et là.
+
+Et on a utilisé les facteurs d'émissions suivants :
+- Facteur 1
+- Facteur 2
+"""
 )
 
 layout = html.Div(
