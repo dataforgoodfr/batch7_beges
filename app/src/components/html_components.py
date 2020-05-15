5aa1253ee83cb89168520a779753856fdcb47299
Ayoub <ayoubsamaki@gmail.com>
2020-05-15 23:44:01 +0200

    Small layout changes and TODO update

--- app/src/components/html_components.py
+++ app/src/components/html_components.py
@@ -8,7 +8,7 @@ import plotly.graph_objects as go
 def build_figure_container(title, id, footer):
     fig_containter = dbc.Card(
         [
-            dbc.CardHeader(html.H4(title), style={"background-color": "#fff"}),
+            dbc.CardHeader(html.H5(title), style={"background-color": "#fff"}),
             dbc.CardBody([dbc.Col(dcc.Graph(id=id))], style={"padding": "0px"}),
             dbc.CardFooter(footer),
         ],
