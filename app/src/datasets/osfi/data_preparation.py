fdf2220cf11a66b74815713abd0930db99aa9d32
Antoine <antoine.biard.10@gmail.com>
2020-05-17 11:40:09 +0200

    Fixing type issues

--- app/src/datasets/osfi/data_preparation.py
+++ app/src/datasets/osfi/data_preparation.py
@@ -6,8 +6,8 @@ GAS_EMISSION_FACTOR = 0.227  # kgCO2e/kWh
 
 
 def add_emission_columns(data):
-    data["emission_electricity"] = ELECTRICITY_EMISSION_FACTOR * data["Consommation d'électricité (kWh)"]
-    data["emission_gaz"] = GAS_EMISSION_FACTOR * data["Consommation de gaz (kWh)"]
+    data["emission_electricity"] = ELECTRICITY_EMISSION_FACTOR * data["Consumption kwh electricity"]
+    data["emission_gaz"] = GAS_EMISSION_FACTOR * data["Gas Consumption (kWh)"]
     return data
 
 
@@ -15,7 +15,30 @@ def load_data(monthly_data_path, buildings_data_path):
     monthly_data = pd.read_csv(monthly_data_path, delimiter=";")
     buildings = pd.read_csv(buildings_data_path, delimiter=";")
     columns_not_in_monthly = list(buildings.columns.difference(monthly_data.columns)) + ["Code bien"]
+    for c in monthly_data.columns:
+        print(c)
+    print()
+    for c in columns_not_in_monthly:
+        print(c)
     data = monthly_data.merge(buildings[columns_not_in_monthly], how="left", left_on="Code bien", right_on="Code bien")
+    return data
+
+
+def convert_string_to_float(x):
+    return float(x.replace(",", ""))
+
+
+def convert_string_to_float_if_string(x):
+    if isinstance(x, str):
+        return convert_string_to_float(x)
+    else:
+        return x
+
+
+def clean_data(data):
+    # Convert numbers in format 2,33 to float (fixing the comma issue)
+    data["Consumption kwh electricity"] = data["Consumption kwh electricity"].apply(convert_string_to_float_if_string)
+    data["Gas Consumption (kWh)"] = data["Gas Consumption (kWh)"].apply(convert_string_to_float_if_string)
     return data
 
 
@@ -24,6 +47,7 @@ def main():
     monthly_data_path = raw_data_path / "osfi_monthly.csv"
     buildings_data_path = raw_data_path / "osfi_buildings.csv"
     data = load_data(monthly_data_path, buildings_data_path)
+    data = clean_data(data)
     data = add_emission_columns(data)
     data.to_csv("/data/cleaned/data_osfi.csv", index=False)
 
