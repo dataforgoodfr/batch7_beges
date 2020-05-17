8218b18ed4cfcf5e0809bc0af285affaddf135a9
Antoine <antoine.biard.10@gmail.com>
2020-05-17 11:20:55 +0200

    Preparing monthly data with buildings

--- app/src/datasets/osfi/data_preparation.py
+++ app/src/datasets/osfi/data_preparation.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import pandas as pd
 
 ELECTRICITY_EMISSION_FACTOR = 0.0571  # kgCO2e/kWh
@@ -10,12 +11,21 @@ def add_emission_columns(data):
     return data
 
 
+def load_data(monthly_data_path, buildings_data_path):
+    monthly_data = pd.read_csv(monthly_data_path, delimiter=";")
+    buildings = pd.read_csv(buildings_data_path, delimiter=";")
+    columns_not_in_monthly = list(buildings.columns.difference(monthly_data.columns)) + ["Code bien"]
+    data = monthly_data.merge(buildings[columns_not_in_monthly], how="left", left_on="Code bien", right_on="Code bien")
+    return data
+
+
 def main():
-    data = pd.read_csv("/data/raw/osfi/osfi.csv")
-    data[["code_batiment", "code_structure"]] = data["Code bien"].str.split("_", expand=True)
-    data["id"] = data["code_structure"] + ";" + data["Région"]
+    raw_data_path = Path("/data/raw/osfi")
+    monthly_data_path = raw_data_path / "osfi_monthly.csv"
+    buildings_data_path = raw_data_path / "osfi_buildings.csv"
+    data = load_data(monthly_data_path, buildings_data_path)
     data = add_emission_columns(data)
-    data.to_csv("/data/cleaned/data_osfi.csv")
+    data.to_csv("/data/cleaned/data_osfi.csv", index=False)
 
 
 if __name__ == "__main__":
