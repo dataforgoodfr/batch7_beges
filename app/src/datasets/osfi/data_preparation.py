c5613600ec1fe1679512f187ae9b59664d7d32dc
Antoine <antoine.biard.10@gmail.com>
2020-05-17 11:20:55 +0200

    Preparing monthly data with buildings

--- app/src/datasets/osfi/data_preparation.py
+++ app/src/datasets/osfi/data_preparation.py
@@ -37,6 +37,14 @@ def clean_data(data):
     return data
 
 
+def load_data(monthly_data_path, buildings_data_path):
+    monthly_data = pd.read_csv(monthly_data_path, delimiter=";")
+    buildings = pd.read_csv(buildings_data_path, delimiter=";")
+    columns_not_in_monthly = list(buildings.columns.difference(monthly_data.columns)) + ["Code bien"]
+    data = monthly_data.merge(buildings[columns_not_in_monthly], how="left", left_on="Code bien", right_on="Code bien")
+    return data
+
+
 def main():
     raw_data_path = Path("/data/raw/osfi")
     monthly_data_path = raw_data_path / "osfi_monthly.csv"
