54b9aa87278fb0e1813b624e548294489f309c23
Tallulah <lula.simone@gmail.com>
2020-05-24 15:33:45 +0200

    Update osfi.py
    added charts for monthly data consumption

--- app/src/apps/osfi.py
+++ app/src/apps/osfi.py
@@ -19,6 +19,24 @@ def get_pie(data, column):
     fig.update_layout(plot_bgcolor="white", template="plotly_white", margin={"t": 30, "r": 30, "l": 30})
     return fig
 
+def get_emissions_timeseries(data, column):
+    biens = data["Nom du bien"].unique()
+    fig = go.Figure()
+    for bien in biens:
+        plot_data = data.loc[data["Nom du bien"] == bien]
+        fig.add_trace(
+            go.Scatter(name=bien,
+                x=plot_data["Date"].astype(str),
+                y=plot_data[column].values,
+                mode="lines+markers",
+                line=dict(width=3),
+            )
+    )
+    fig.update_layout(
+        plot_bgcolor="white", template="plotly_white", margin={"t": 30, "r": 30, "l": 30}#, xaxis=xaxis_format
+    )
+    return fig
+
 
 layout = html.Div(
     [
@@ -46,6 +64,22 @@ layout = html.Div(
                 width=12,
             )
         ),
+        dbc.Row(
+            dbc.Col(
+                build_figure_container(
+                    title="Évolution temporelles des émissions (électricité)", id="electricity_time_series", footer="Explications..."
+                ),
+                width=12,
+            )
+        ),
+        dbc.Row(
+            dbc.Col(
+                build_figure_container(
+                    title="Évolution temporelles des émissions (gaz)", id="gaz_time_series", footer="Explications..."
+                ),
+                width=12,
+            )
+        ),
     ]
 )
 
@@ -64,8 +98,8 @@ layout = html.Div(
 def update_graphs(selected_entity):
     service = oc.get_entity_by_id(selected_entity)
     data = oh.get_structure_data(service.code_osfi)
-    #columns = [{"name": i, "id": i} for i in data.columns]
-    columns = [{"name": i, "id": i} for i in data.loc[:,["Nom du bien","Date"]]]
+    columns = [{"name": i, "id": i} for i in data.columns]
+    #columns = [{"name": i, "id": i} for i in data.loc[:,["Nom du bien","Date"]]]
     row_selectable = "multi"
     selected_rows=[]
     style_data={'whiteSpace': 'normal','height': 'auto','minWidth': '60px', 'width': '180px', 'maxWidth': '180px'}
@@ -78,6 +112,8 @@ def update_graphs(selected_entity):
     [
         Output("emission-electricity-pie", "figure"),
         Output("emission-gas-pie", "figure"),
+        Output("electricity_time_series", "figure"),
+        Output("gaz_time_series", "figure"),
     ],
     [
         Input('osfi-all-data-table', "derived_virtual_data"),
@@ -87,17 +123,22 @@ def update_graphs(selected_entity):
     ],
     )
 def update_graphs_selected(derived_virtual_data, derived_virtual_selected_rows, selected_rows,data):
-
     if derived_virtual_selected_rows is None:
         derived_virtual_selected_rows = []
     if selected_rows is None:
         selected_rows = []
     selected_rows = [data[int(i)] for i in selected_rows]
     selected_rows = pd.DataFrame(selected_rows)
+    data = pd.DataFrame(data)
     if len(selected_rows) > 0:
         electricity_pie_graph = get_pie(selected_rows, "emission_electricity")
         gas_pie_graph = get_pie(selected_rows, "emission_gaz")
-    else:
+        electricity_time_series = get_emissions_timeseries(selected_rows, "emission_electricity")
+        gaz_time_series = get_emissions_timeseries(selected_rows, "emission_gaz")
+    else: 
         electricity_pie_graph = get_pie(pd.DataFrame(data), "emission_electricity")
         gas_pie_graph = get_pie(pd.DataFrame(data), "emission_gaz")
-    return electricity_pie_graph, gas_pie_graph
+        electricity_time_series = get_emissions_timeseries(data, "emission_electricity")
+        gaz_time_series = get_emissions_timeseries(data, "emission_gaz")
+    return electricity_pie_graph, gas_pie_graph, electricity_time_series, gaz_time_series
+
