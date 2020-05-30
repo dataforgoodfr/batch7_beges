9b3b2c5da5ef31e735a0f84d74268bdad49d8bc9
ArtusVuatrin <a.vuatrin@yahoo.fr>
2020-05-30 23:35:28 +0200

    Changes on odrive dashboard

--- app/src/utils/odrive_handler.py
+++ app/src/utils/odrive_handler.py
@@ -9,10 +9,15 @@ class OdriveHandler:
     def __init__(self):
         self.data = pd.read_csv("/data/cleaned/data_odrive.csv")
 
-    def get_structure_data(self, code_structure=None):
+    def get_structure_data(self, code_structure=None, filter_motorisation=None, filter_emissions=None):
+        temp = self.data
+        if filter_motorisation is not None:
+            temp = temp.loc[temp["Motorisation"] == code_structure]
         if code_structure is not None:
-            return self.data.loc[self.data["Entité 2"] == code_structure, :]
-        return self.data
+            temp = temp.loc[temp["Entité 2"] == code_structure, :]
+        if filter_emissions is not None:
+            temp = temp.loc[filter_emissions[1] >= temp["CO2 (g/km)"] >= filter_emissions[0], :]
+        return temp
 
 
 ov = OdriveHandler()
