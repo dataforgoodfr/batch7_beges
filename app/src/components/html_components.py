30ba8a1c99078ba0a4969b61d66928b9302108ef
antoan2 <antoine.biard.10@gmail.com>
2020-06-05 19:25:27 +0200

    Add graph config

--- app/src/components/html_components.py
+++ app/src/components/html_components.py
@@ -6,7 +6,11 @@ import dash_bootstrap_components as dbc
 
 def build_figure_container(title, id, footer=None):
     fig_containter = dbc.Card(
-        [dbc.CardHeader(html.H4(title)), dbc.CardBody([dbc.Col(dcc.Graph(id=id))]), dbc.CardFooter(footer),],
+        [
+            dbc.CardHeader(html.H4(title)),
+            dbc.CardBody([dbc.Col(dcc.Graph(id=id, config={"toImageButtonOptions": {"filename": id + ".png"}}))]),
+            dbc.CardFooter(footer),
+        ],
         className="m-2 pretty_container",
     )
     return fig_containter
