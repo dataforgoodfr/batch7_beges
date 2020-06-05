b4eb49bd182a8aa5e5e2bc1c39fe20975f08efb5
antoan2 <antoine.biard.10@gmail.com>
2020-06-05 19:24:41 +0200

    Clean up

--- app/src/apps/equipe.py
+++ app/src/apps/equipe.py
@@ -3,14 +3,14 @@ import dash_html_components as html
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 
-def get_team_member_card(name, title=None, linkedin=None, github=None):
+def get_team_member_card(name, image=None,title=None, linkedin=None, github=None):
     return dbc.Card( [
-        dbc.CardImg(src="/static/images/test.jpg", top=True),
+        dbc.CardImg(src=image, top=True),
         dbc.CardBody(
             [
             html.H5(name, className="card-title"),
             html.I(title),
-            html.P( 'Small test', className='card-text')
+            #html.P( 'Small test', className='card-text')
             ],
             style={'text-align': 'center'}
 
@@ -24,11 +24,11 @@ layout = dbc.Row(
         html.H1('Les porteurs du projet', className='m-5'),
         dbc.Row( [
             dbc.Col(
-                get_team_member_card(name='Anthony Dicanot'),
+                get_team_member_card(name='Anthony Dicanot', image='https://media-exp1.licdn.com/dms/image/C5603AQHvkNFPa9bSBg/profile-displayphoto-shrink_200_200/0?e=1596672000&v=beta&t=P2CPd8PrFG-W5RabbzWjF1BSZfoznTLnpmfEJBd8u6M', title="MTES"),
                 width={'offset': 2, 'size': 4},
             ),
             dbc.Col(
-                get_team_member_card(name='Maxime Roux'),
+                get_team_member_card(name='Maxime Roux', title="MTES"),
                 width=4
             ),
         ]),
